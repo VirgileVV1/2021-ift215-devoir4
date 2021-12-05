56a6aed843ebb7c2c1adc5923e70cbce826937be
VirgileVV1 <90397556+VirgileVV1@users.noreply.github.com>
2021-12-04 20:33:55 -0500

    Update inscription.js

--- pro-gramme/client/scripts/inscription.js
+++ pro-gramme/client/scripts/inscription.js
@@ -1,13 +1,74 @@
+function sinscrire() {
 
-
-function attacherListenerMenuGauche(){
-    let menu = document.getElementById("menu-gauche");
-    let liens = menu.children;
-    for (let i = 0 ; i<liens.length ; i++){
-        liens[i].addEventListener('click', function(){
-            menuGaucheClic(liens[i])
-        });
+    const formulaire = new FormData(document.getElementById('form-inscription'));
+    const corps = JSON.stringify(Object.fromEntries(formulaire));
+    const init = {
+        method: 'POST',
+        body: corps,
+        headers: {'Content-type': 'application/json; charset=UTF-8'}
+    };
+    requestOk = true
+    corps_json = JSON.parse(corps)
+    try {
+        if (corps_json.prenom == "") {
+            throw new Error("Le champ prénom ne peut pas être vide")
+        }
+        if (corps_json.nom == "") {
+            throw new Error("Le champ nom ne peut pas être vide")
+        }
+        if (corps_json.age == "") {
+            throw new Error("Le champ âge ne peut pas être vide")
+        }
+        if (corps_json.courriel == "") {
+            throw new Error("Le champ adresse éléctronique ne peut pas être vide")
+        }
+        if (corps_json.mdp == "") {
+            throw new Error("Le champ mot de passe ne peut pas être vide")
+        }
+        if (corps_json.adresse == "") {
+            throw new Error("Le champ adresse ne peut pas être vide")
+        }if (corps_json.pays == "") {
+            throw new Error("Le champ pays ne peut pas être vide")
+        }
     }
+    catch (error) {
+        requestOk = false
+        afficherMsgErreur(error);
+    }
+    if (requestOk) {
+        fetch('/clients', init)
+            .then(reponse => {
+                if (reponse.ok) {
+                    return reponse.json();
+                } else {
+
+                    if (reponse.status == 400) {
+                        throw new Error("L'utilisateur existe déjà" );
+                    } else {
+                        throw new Error(reponse.statusText)
+                    }
+                    //console.log(response.text().then());
+                    return reponse.text();
+                }
+            })
+            .then(json => {
+                if (typeof json === 'object' && json !== null) {
+                    console.log('Reussi');
+                    afficherMessage('Inscription réussie!', 'positif');
+                } else {
+                    console.log(json);
+                    afficherMessage(`Erreur: ${json}`, 'negatif');
+                    // document.getElementById('erreur-i').innerHTML = "<p> ERROR </p>";
+
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                afficherMsgErreur(err);
+
+            });
+    }
+
 }
 
 /**
@@ -15,37 +76,14 @@ function attacherListenerMenuGauche(){
  * Remplace le DOMContentLoaded qui est lancé bien avant que le contenu associé à ce script ne soit dans l'écran.
  * @returns {Promise<void>}
  */
-async function chargerinscription (){
-    attacherListenerMenuGauche()
-}
+async function chargerinscription () {
+    const btnInscription = document.getElementById('btn-sinscrire');
+    btnInscription.addEventListener('click', sinscrire);
+};
 
-function inscription() {
-    const formulaire = new FormData(document.getElementById('form-inscription'));
-    const corps = JSON.stringify(Object.fromEntries(formulaire));
-    const init = {
-        method: 'POST',
-        body: corps,
-        headers: { 'Content-type': 'application/json; charset=UTF-8' }
-    };
-    fetch('/clients', init)
-        .then(reponse => {
-            if (reponse.ok) {
-                return reponse.json();
-            } else {
-                return reponse.text();
-            }
-        })
-        .then(json => {
-            if (typeof json === 'object' && json !== null) {
-                console.log('Reussi');
-                afficherMessage('Inscription réussie!', 'positif');
-            } else {
-                console.log(json);
-                afficherMessage(`Erreur: ${json}`, 'negatif');
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
+function afficherMsgErreurInscr(erreur) {
+    console.log(erreur)
+    err = erreur.toString().split(': ')
+    document.getElementById('erreur-i').innerHTML = "<p> erreur: le champ \""+err[1]+"\" ne peut pas être vide </p>"
 
 }
