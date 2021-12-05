f4a09b155719e74a213fc758072fdb8740c7b101
Angele_Roussel <angele.roussel@etu.univ-tours.fr>
2021-12-05 17:15:21 -0500

    page point de vente

--- pro-gramme/client/scripts/points_de_vente.js
+++ pro-gramme/client/scripts/points_de_vente.js
@@ -1,55 +1,25 @@
 
 function genererMagasin(data){
-    let locaux = document.getElementById('pdv-grille');
-    let internationaux = document.getElementById('pdv-flex');
+    let locaux = document.getElementById('canada');
+    let internationaux = document.getElementById('international');
 
-    let innerHTML = ""
+    let innerHTML = "<h2>Au Canada:</h2>";
     for(let indice in data.commerces_locaux){
-        let element = data.commerces_locaux[indice]
-        innerHTML += "<article class=\"pdv-item\">" +
-                "<h1>" + element.ville + "</h1>" +
-                "<address>"+ element.adresse + "</address>" +
-                "<h2>Heures d'ouverture: </h2>" +
-                "<ul>";
-        for (let h in element.horaire){
-            let jour = element.horaire[h];
-            innerHTML += "<li>" + jour + "</li>";
-        }
-
-        innerHTML += "</ul></article>";
-
+        let element = data.commerces_locaux[indice];
+        innerHTML += "<article>"+element.adresse+
+            ", "+element.ville+", "+element.pays+
+            "</article>";
     }
-
     locaux.innerHTML = innerHTML;
 
+    innerHTML = "<h2>A l'étranger:</h2>";
     for(let indice in data.commerces_internationaux){
         let element = data.commerces_internationaux[indice];
-        let article = document.createElement('article');
-        article.classList.add('pdv-item');
-
-        let h1 = document.createElement('h1');
-        h1.innerText = element.ville;
-        article.appendChild(h1);
-
-        let adresse = document.createElement('address');
-        adresse.innerText = element.adresse;
-        article.appendChild(adresse);
-
-        let h2 = document.createElement('h2');
-        h2.innerText = "Heures d'ouverture:";
-        article.appendChild(h2);
-
-        let ul = document.createElement('ul');
-        for (let h in element.horaire){
-            let jour = element.horaire[h];
-            let li = document.createElement('li');
-            li.innerText = jour;
-            ul.appendChild(li);
-        }
-        article.appendChild(ul)
-
-        internationaux.appendChild(article)
+        innerHTML += "<article>"+element.adresse+
+            ", "+element.ville+", "+element.pays+
+            "</article>";
     }
+    internationaux.innerHTML = innerHTML;
 
 }
 
